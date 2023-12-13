946caeebeb064f894635b98a327cb5fe42b6f1cd
Oliver Rushworth <mail@oliverrushworth.uk>
2023-12-13 23:47:53 +0000

    Day 13 part 2

--- day-13/day-13.ts
+++ day-13/day-13.ts
@@ -12,7 +12,7 @@ function sliceGridFromMidpoint(midpoint: number, grid: Grid): Grid {
   return grid.slice(start, end);
 }
 
-function isGridSymmetrical(grid: Grid): boolean {
+function isGridSymmetrical(grid: Grid, expectedDifferences: number): boolean {
   if (grid.length === 0) {
     return false;
   }
@@ -20,31 +20,44 @@ function isGridSymmetrical(grid: Grid): boolean {
   const midpoint = grid.length / 2;
   const leftSide = grid.slice(0, midpoint).toReversed();
   const rightSide = grid.slice(midpoint);
+  const differences = differencesBetweenArrays(
+    leftSide.flat(),
+    rightSide.flat()
+  );
 
-  return Bun.deepEquals(leftSide, rightSide);
+  return differences === expectedDifferences;
 }
 
-function findGridMidpoint(grid: Grid): number {
-  for (let midpoint of range(0, grid.length)) {
-    const subGridWithMidpoint = sliceGridFromMidpoint(midpoint, grid);
+function differencesBetweenArrays<T>(a: T[], b: T[]): number {
+  let differences = 0;
 
-    if (isGridSymmetrical(subGridWithMidpoint)) {
-      return midpoint;
+  for (let index of a.keys()) {
+    if (a[index] !== b[index]) {
+      differences++;
+    }
+  }
+
+  return differences;
+}
+
+function findGridMidpoint(grid: Grid, expectedDifferences: number): number {
+  for (let index of grid.keys()) {
+    const subGridWithMidpoint = sliceGridFromMidpoint(index, grid);
+
+    if (isGridSymmetrical(subGridWithMidpoint, expectedDifferences)) {
+      return index;
     }
   }
 
   return 0;
 }
 
-function* range(start: number, end: number): Generator<number> {
-  for (let i = start; i <= end; i++) {
-    yield i;
-  }
-}
-
-function getGridPoints(grid: Grid, gridIndex: number): number {
-  const columnsBeforeMidpoint = findGridMidpoint(getGridColumns(grid));
-  const rowsBeforeMidpoint = findGridMidpoint(grid);
+function getGridPoints(grid: Grid, expectedDifferences: number): number {
+  const rowsBeforeMidpoint = findGridMidpoint(grid, expectedDifferences);
+  const columnsBeforeMidpoint = findGridMidpoint(
+    getGridColumns(grid),
+    expectedDifferences
+  );
 
   return rowsBeforeMidpoint * 100 + columnsBeforeMidpoint;
 }
@@ -58,7 +71,13 @@ const input = await Bun.file("input.txt").text();
 const part1 = input
   .split("\n\n")
   .map(parseGrid)
-  .map(getGridPoints)
+  .map((grid) => getGridPoints(grid, 0))
   .reduce((total, current) => total + current, 0);
 
-console.log({ part1 });
+const part2 = input
+  .split("\n\n")
+  .map(parseGrid)
+  .map((grid) => getGridPoints(grid, 1))
+  .reduce((total, current) => total + current, 0);
+
+console.log({ part1, part2 });
